527d13d41b07904b79cc5384e006433af2405dd4
Nathanael Langlois <nathanael.langlois@gmail.com>
2016-02-10 14:53:38 +0100

    Added MySQL requirement

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SapphireORM
-A Julia ORM
+A Julia ORM for MySQL only.
+
+You need to have MySQL installed on your computer and type :
+Pkg.clone("https://github.com/JuliaComputing/MySQL.jl")
--- src/SapphireORM.jl
+++ src/SapphireORM.jl
@@ -1,3 +1,15 @@
 module SapphireORM
 
+using MySQL
+
+function connect(host::AbstractString,username::AbstractString,password::AbstractString,db_name::AbstractString)
+  mysql_connect(host,username,password,db_name)
+end
+
+function get() end
+function getAll() end
+function update() end
+function updateAll() end
+function insert() end
+
 end # end of module
