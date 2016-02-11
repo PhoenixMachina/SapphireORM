59635aa50e591624bc512807e441506a2665536c
Neabfi <fabien.delattre@outlook.com>
2016-02-11 13:03:58 +0100

    Replacing Type with simple function

--- src/SapphireORM.jl
+++ src/SapphireORM.jl
@@ -1,38 +1,32 @@
 module SapphireORM
 
   using ODBC
-
   export Sapphire
 
-  type Sapphire
-    conn::ODBC.Connection
-    function Sapphire(;username=nothing,
+  function Sapphire(;username=nothing,
                           password=nothing,
                           dbname=nothing,
                           option = 3,
                           host = "localhost",
                           driver = "MySQL ODBC 5.3 Unicode Driver")
-
-      conn = advancedconnect("Driver={$driver};Server=$host;Database=$dbname;User=$username;Password=$password;Option=$option;")
-      new(conn)
-    end
+    return advancedconnect("Driver={$driver};Server=$host;Database=$dbname;User=$username;Password=$password;Option=$option;")
   end
 
-  function get(sapphire::Sapphire;
+  function get(conn::Connection;
                table=nothing,
-              values = [])
+               values = [])
 
     if length(values) > 0
       values = join(values, ",")
     else
       values = "*"
     end
-    query("SELECT $values FROM $table")
+    query("SELECT $values FROM $table", conn)
     return conn.resultset
   end
 
-  function update(::Sapphire) end
+  function update(::Connection) end
 
-  function insert(::Sapphire) end
+  function insert(::Connection) end
 
 end
--- src/test.jl
+++
@@ -1,7 +0,0 @@
-include("SapphireORM.jl")
-
-using SapphireORM
-
-sapphire = Sapphire(username="root", password="tututa", dbname="test");
-
-SapphireORM.get(sapphire, table="toto", values=[])
