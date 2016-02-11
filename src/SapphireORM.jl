8c37faed39491b3f391b5be7d74fe552317ff3f4
Neabfi <fabien.delattre@outlook.com>
2016-02-11 15:55:14 +0100

    Passing args via Dict

--- src/SapphireORM.jl
+++ src/SapphireORM.jl
@@ -3,30 +3,63 @@ module SapphireORM
   using ODBC
   export Sapphire
 
-  function Sapphire(;username=nothing,
-                          password=nothing,
-                          dbname=nothing,
+  # Sapphire
+  function Sapphire(;username = nothing,
+                          password = nothing,
+                          dbname = nothing,
                           option = 3,
                           host = "localhost",
                           driver = "MySQL ODBC 5.3 Unicode Driver")
+
     return advancedconnect("Driver={$driver};Server=$host;Database=$dbname;User=$username;Password=$password;Option=$option;")
+
   end
 
-  function get(conn::Connection;
-               table=nothing,
-               values = [])
+  # GET
+  function get(conn::Connection, params::Dict=Dict())
 
-    if length(values) > 0
-      values = join(values, ",")
+    # Table
+    if haskey(params, "table")
+      table = params["table"]
+    else
+      error("No table specify")
+    end
+
+    # Values
+    if haskey(params, "values") && length(params["values"]) > 0
+      values = join(params["values"], ",")
     else
       values = "*"
     end
+
     query("SELECT $values FROM $table", conn)
+    return conn.resultset
+
+  end
+
+
+  # INSERT
+  function insert(conn::Connection, params::Dict=Dict())
+
+    # Table
+    if haskey(params, "table")
+      table = params["table"]
+    else
+      error("No table specify")
+    end
+
+    # Values
+    if haskey(params, "values") && length(params["values"]) > 0
+      values = join(params["values"], ",")
+    else
+      values = "*"
+    end
+
+    query("INSERT INTO $table VALUES($values)", conn)
     return conn.resultset
   end
 
+  # UPDATE
   function update(::Connection) end
-
-  function insert(::Connection) end
 
 end
